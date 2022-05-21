9a29d80dd4356b6cb6e16cb778d7ac57a9193f2a
djordjekrsmanovic <73399785+djordjekrsmanovic@users.noreply.github.com>
2022-05-21 15:44:31 +0200

    feat:ada

---
+++ auth_service/domain/passwordless_token.go
@@ -0,0 +1,13 @@
+package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type PasswordlessToken struct {
+	Id           primitive.ObjectID `bson:"_id,omitempty"`
+	UserId       string             `bson:"user_id"`
+	TokenCode    string             `bson:"token_code"`
+	CreationDate time.Time          `bson:"creation_date"`
+}
