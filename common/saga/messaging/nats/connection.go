5b32d2fcbb80c12df2bf8dee0e8433fc1ed46a4c
Tara Pogančev <tarapogancev@gmail.com>
2022-06-29 15:26:20 +0200

    feat: docker nats

--- common/saga/messaging/nats/connection.go
+++ common/saga/messaging/nats/connection.go
@@ -1,12 +1,14 @@
 package nats
 
 import (
+	"fmt"
 	"github.com/nats-io/nats.go"
 )
 
 func getConnection(host, port, user, password string) (*nats.Conn, error) {
-	//url := fmt.Sprintf("nats://%s:%s@%s:%s", user, password, host, port)
-	connection, err := nats.Connect(nats.DefaultURL)
+	url := fmt.Sprintf("nats://%s:%s@%s:%s", user, password, host, port)
+	//connection, err := nats.Connect(nats.DefaultURL)
+	connection, err := nats.Connect(url)
 	if err != nil {
 		return nil, err
 	}
